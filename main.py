09d8427b9bb75061a8540a91937ec37de97759bc
kobi <kobikahn@gmail.com>
2024-04-16 21:34:43 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# Import random folder and maybe hebrew if needed
+# Import txt file
+# If there is not a dictionary in use then create new one, else: check for new words in vocab text file to add to dictionary
+# Import all new words to dictionary     {hebrew_word:english_word:rank_h:rank_e:}
+# Assign each new word a rank for english and hebrew answers with each word starting at 0 and can go up to 4
+# Shuffle all the words every time the user wants to study
+# Have a choice of Answering in English, Hebrew, or Alternating (through random)
+# Have different functions for each possibility
+# Basic input and all caps so can't mess up spelling
+#{
+# If correct the word goes up a rank and print that rank
+# Print Yay you got it!
+# If incorrect the word goes down 2 ranks (or 0 if it cant go lower)
+# Print oh no youre wrong
+# Show correct answer and print that rank
+#}
+# User presses continue
+# After doing all of the words there is a choice to keep reviewing and it repeats with the new dictionary or you can choose to stop and it saves your dictionary
+# Have a choice to save in the middle of learning if you quickly have to go and stop and it saves your dictionary in another tab
+# Seperate py or txt file to save dictionary and just import it if it is
+# After each session create an organized list of all the words and their ranks from best to worst in both categories separately
+
+
+
+
+
+
